3995eeda9393610952dd3641e90c7d4d565016fe
Chris C <cchockster@gmail.com>
2015-02-10 21:44:18 -1000

    Working om TWI

--- nRF51422/LightSensor.c
+++ nRF51422/LightSensor.c
@@ -24,37 +24,12 @@
 uint8_t scratch = 0;
 
 void init_light_sensor(){
-//	/* Init the A0 pin to 0 */
-//	nrf_gpio_cfg_output(MAX_44008_A0);
-//	nrf_gpio_pin_set(MAX_44008_A0);
-
-//	nrf_gpio_cfg_output(MAX_44008_INT);
-//	nrf_gpio_pin_clear(MAX_44008_INT);
-//	nrf_delay_ms(20);
-//	nrf_gpio_pin_set(MAX_44008_INT);
-
-	nrf_gpio_cfg_output(MAX_44008_SCL);
-	nrf_gpio_pin_clear(MAX_44008_SCL);
-	nrf_delay_ms(20);
-	nrf_gpio_pin_set(MAX_44008_SCL);
-
-	nrf_gpio_cfg_output(MAX_44008_SDA);
-	nrf_gpio_pin_clear(MAX_44008_SDA);
-	nrf_delay_ms(20);
-	nrf_gpio_pin_set(MAX_44008_SDA);
-
-	nrf_delay_ms(100);
-
 	/* Init the /INT pin to input */
 	nrf_gpio_cfg_input(MAX_44008_INT, NRF_GPIO_PIN_NOPULL);
 
 	/* Init TWI master driver */
 	if(twi_master_init())
 		LED_TURN_ON(LED_GREEN);
-
-//	scratch = 0xA5;
-//    twi_master_transfer(0x55, &scratch, 1, TWI_ISSUE_STOP);
-
 	
 	/*  */
 	read_int_register();
